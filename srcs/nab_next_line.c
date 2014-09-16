cc158bdc5ac9cfcb09529f52fae153f1ed1a0a0f
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2014-09-16 18:07:38 +0200

    nab_next_line.c optimized

--- srcs/nab_next_line.c
+++ srcs/nab_next_line.c
@@ -12,17 +12,19 @@
 
 #include	"bsq.h"
 
-size_t	nab_next_line(int const fd, char **line, char **overread)
+size_t	nab_next_line(int const fd, char **line, char **spill)
 {
 	size_t			line_len;
 	size_t			ret;
 	char			buf[BUF_SIZE + 1];
 	size_t			cut_len;
 	char			*line_cut;
-	//static char	overread[BUF_SIZE + 1];
 
-	*line = "";
-	line_len = 0;
+	line_len = ft_strlen(*spill);
+	if (line_len > 0 && ft_strlen(*line = str_slice(*spill, '\n')) < line_len)
+		return (line_len);
+	else
+		*line = "";
 	while ((ret = read(fd, buf, BUF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
@@ -31,10 +33,10 @@ size_t	nab_next_line(int const fd, char **line, char **overread)
 		ft_strappend(line, line_len, line_cut, cut_len);
 		line_len += cut_len;
 		if (cut_len < ret)
-			break;
+			break ;
 	}
 	if (ret == -1)
 		exit(ft_puterror("read()", "line N/A"));
-	*overread = buf;
+	*spill = buf;
 	return (line_len);
 }
