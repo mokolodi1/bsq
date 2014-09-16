74c5780e505ac6596c83828fbb99ddf9ed5d1caf
tfleming <mokolodi1@gmail.com>
2014-09-16 19:05:38 +0200

    fixed print_terrain.c

--- srcs/print_terrain.c
+++ srcs/print_terrain.c
@@ -6,7 +6,7 @@
 /*   By: tfleming <tfleming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/16 16:28:44 by tfleming          #+#    #+#             */
-/*   Updated: 2014/09/16 16:31:13 by tfleming         ###   ########.fr       */
+/*   Updated: 2014/09/16 18:48:32 by tfleming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ void		print_terrain(t_terrain *terrain)
 	size_t inner;
 
 	outer = 0;
-	while (outer < board->height)
+	while (outer < terrain->height)
 	{
 		inner = 0;
-		while (inner < board->width)
+		while (inner < terrain->width)
 		{
 			ft_putchar(terrain->map[outer][inner]);
 			inner++;
