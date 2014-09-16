b37101853f5249f637221df9a1e789a6fb67e784
tfleming <mokolodi1@gmail.com>
2014-09-16 18:13:36 +0200

    changed gen_boards.c (wrote functions)

--- srcs/gen_boards.c
+++ srcs/gen_boards.c
@@ -6,7 +6,7 @@
 /*   By: tfleming <tfleming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/16 13:56:46 by tfleming          #+#    #+#             */
-/*   Updated: 2014/09/16 14:24:11 by tfleming         ###   ########.fr       */
+/*   Updated: 2014/09/16 18:12:54 by tfleming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,30 +36,62 @@ t_board		*init_board(t_terrain *terrain)
 /*
 **	pay_as_you_go
 **	Calculates the pay-as-you-go board
-**	TODO
 */
 
 t_board		*pay_as_you_go(t_terrain *terrain)
 {
 	t_board		*new_board;
+	size_t		out;
+	size_t		in;
+	int			curr;
 
 	new_board = init_board(terrain);
-	
+	out = 0;
+	while (out < terrain->height)
+	{
+		in = 0;
+		while (in < terrain->width)
+		{
+			if (terrain->map[out][in] == t_terrain->obstacle)
+				curr++;
+			new_board->map[out][in] = curr;
+			in++;
+		}
+		out++;
+	}
+		
 	return (new_board);
 }
 
 /*
 **	check_the_map
 **	Calculates the check-the-map board
-**	TODO
 */
 
 t_board		*check_the_map(t_terrain *terrain)
 {
 	t_board		*new_board;
+	size_t		out;
+	size_t		in;
+	int			curr;
 
 	new_board = init_board(terrain);
-
+	out = 0;
+	while (out < terrain->height - 1)
+	{
+		in = terrain->width;
+		curr = 0;
+		while (in >= 0)
+		{
+			if (terrain->map[out][in] == t_terrain->obstacle)
+				curr = 0;
+			else
+				curr++;
+			new_board->map[out][in] = curr;
+			in--;
+		}
+		out++;
+	}
 	return (new_board);
 }
 
@@ -74,6 +106,6 @@ t_board		*check_the_map_up(t_terrain *terrain)
 	t_board		*new_board;
 
 	new_board = init_board(terrain);
-	
+	ft_putstr("check_the_map_up: NOT IMPLEMENTED YET\n");
 	return (new_board);
 }
