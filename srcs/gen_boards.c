de77510c5cd119efc97d7ed1722e679dfe5db2a5
tfleming <mokolodi1@gmail.com>
2014-09-17 11:11:40 +0200

    fixed gen_boards.c

--- srcs/gen_boards.c
+++ srcs/gen_boards.c
@@ -6,7 +6,7 @@
 /*   By: tfleming <tfleming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/16 13:56:46 by tfleming          #+#    #+#             */
-/*   Updated: 2014/09/16 19:31:33 by tfleming         ###   ########.fr       */
+/*   Updated: 2014/09/16 23:25:10 by tfleming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 **	Will never call from anywhere else: not in .h
 */
 
-t_board		*init_board(t_terrain *terrain)
+t_board		*init_board(t_terrain *terrain, int extra_w)
 {
 	t_board		*new_board;
 	size_t		i;
@@ -26,13 +26,17 @@ t_board		*init_board(t_terrain *terrain)
 	new_board = malloc(sizeof(t_board));
 	if (!new_board)
 		exit(ft_puterror("init_board()", "Not Enough Memory"));
-	new_board->width = terrain->width;
+	new_board->width = terrain->width + extra_w;
 	new_board->height = terrain->height;
 	new_board->map = malloc(sizeof(int*) * new_board->height);
+	if (!new_board)
+		exit(ft_puterror("init_board()", "Not Enough Memory"));
 	i = 0;
 	while (i < new_board->height)
 	{
-		new_board->map[i] = malloc(sizeof(int) * new_board->width);
+		new_board->map[i] = malloc(sizeof(int) * new_board->width + extra_w);
+		if (!new_board)
+			exit(ft_puterror("init_board()", "Not Enough Memory"));
 		i++;
 	}
 	return (new_board);
@@ -41,24 +45,25 @@ t_board		*init_board(t_terrain *terrain)
 /*
 **	pay_as_you_go
 **	Calculates the pay-as-you-go board
+**	Note: off-by-one width
 */
 
-t_board		*pay_as_you_go(t_terrain *terrain)
+t_board		*pay_as_you_go_board(t_terrain *terrain)
 {
 	t_board		*new_board;
 	size_t		out;
 	size_t		in;
 	int			curr;
 
-	new_board = init_board(terrain);
+	new_board = init_board(terrain, 1);
 	out = 0;
-	while (out < terrain->height)
+	while (out < new_board->height)
 	{
-		in = 0;
+		in = 1;
 		curr = 0;
-		while (in < terrain->width)
+		while (in < new_board->width)
 		{
-			if (terrain->map[out][in] == terrain->obstacle)
+			if (terrain->map[out][in - 1] == terrain->obstacle)
 				curr++;
 			new_board->map[out][in] = curr;
 			in++;
@@ -71,16 +76,17 @@ t_board		*pay_as_you_go(t_terrain *terrain)
 /*
 **	check_the_map
 **	Calculates the check-the-map board
+**	TODO: add extra on right
 */
 
-t_board		*check_the_map(t_terrain *terrain)
+t_board		*check_the_map_board(t_terrain *terrain)
 {
 	t_board		*new_board;
 	size_t		out;
 	size_t		in;
 	int			curr;
 
-	new_board = init_board(terrain);
+	new_board = init_board(terrain, 1);
 	out = 0;
 	while (out < terrain->height)
 	{
@@ -110,7 +116,7 @@ t_board		*check_the_map_up(t_terrain *terrain)
 {
 	t_board		*new_board;
 
-	new_board = init_board(terrain);
+	new_board = init_board(terrain, 0);
 	ft_putstr("check_the_map_up: NOT IMPLEMENTED YET\n");
 	return (new_board);
 }
