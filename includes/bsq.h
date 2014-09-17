308f2d7bc8f5920ecb70113b213d86ed5989fa8f
tfleming <mokolodi1@gmail.com>
2014-09-17 11:10:27 +0200

    fixed bsq.h

--- includes/bsq.h
+++ includes/bsq.h
@@ -6,7 +6,7 @@
 /*   By: tfleming <tfleming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/09/16 13:47:22 by tfleming          #+#    #+#             */
-/*   Updated: 2014/09/16 18:17:15 by tfleming         ###   ########.fr       */
+/*   Updated: 2014/09/16 22:33:48 by tfleming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,30 @@ typedef struct		s_board
 	size_t	height;
 }					t_board;
 
+/*
+**	Holds a solution
+*/
+
+typedef struct		s_solution
+{
+	int		row;
+	int		col;
+	int		size;
+}					t_solution;
+
 size_t				nab_next_line(int const fd, char **line, char **spill);
 char				*str_slice(char *fish, char knife);
-void				ft_strappend(char **str, size_t str_len, char *add, size_t add_len);
+void				ft_strappend(char **str, size_t str_len
+									, char *add, size_t add_len);
 void				print_terrain(t_terrain *terrain);
 void				print_board(t_board *board, int num_width);
-t_board				*init_board(t_terrain *terrain);
-t_board				*pay_as_you_go(t_terrain *terrain);
+t_board				*init_board(t_terrain *terrain, int extra_w);
 t_board				*check_the_map(t_terrain *terrain);
+t_solution			*pay_as_you_go(t_board *board);
+t_solution			*make_solution(int row, int col, int size);
+t_board				*pay_as_you_go_board(t_terrain *terrain);
+t_board				*check_the_map_board(t_terrain *terrain);
+void				fill_solution(t_terrain *terrain, t_solution *solution);
 
 #endif
 
